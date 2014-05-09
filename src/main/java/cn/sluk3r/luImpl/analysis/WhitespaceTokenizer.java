2e186dae53424fb05a2837e6ca352b3ec6ff736a
sluk3r <sluk3r@gmail.com>
2014-05-09 07:52:55 +0000

    WhitespaceTokenizer 全部实现

--- src/main/java/cn/sluk3r/luImpl/analysis/WhitespaceTokenizer.java
+++ src/main/java/cn/sluk3r/luImpl/analysis/WhitespaceTokenizer.java
@@ -7,12 +7,15 @@ import java.io.Reader;
  * Created by wangxc on 2014/5/9.
  */
 public class WhitespaceTokenizer implements TokenStream {
-    private static char BLANK_CHAR = '\u0000';
+    private static int IO_BUFFER_SIZE = 1024;
+    private static final int MAX_WORK_LENGTH = 255;
 
-    private static int IO_BUFFER_SIZE = 4096;
     private char[] ioBuffer = new char[IO_BUFFER_SIZE];
-    private int bufferIndex, offset = 0;
-    private int lastBlankIndex = 0;
+    private char[] resultBuffer = new char[MAX_WORK_LENGTH];
+    int resultBufferLength = 0;
+    int dataLen = 0;
+    int bufferIndex = 0;
+
 
     Reader in;
     public WhitespaceTokenizer(Reader reader) {
@@ -21,115 +24,38 @@ public class WhitespaceTokenizer implements TokenStream {
 
     @Override
     public boolean incrementToken() throws IOException {
-        if (! bufferEmpty(ioBuffer)) {//缓存里已有时， 直接从缓存里读。
-            if (ioBuffer[offset] == BLANK_CHAR) {
+        while(true) {
+            if (bufferIndex >= dataLen) {
+                dataLen = in.read(ioBuffer);
+                bufferIndex = 0;
+            }
+
+            if (dataLen == -1) {
+                dataLen = 0; //wangxc 下次循环时， 再从in里读一次，这时resultBufferLength肯定是0， 也就return false了。  TODO, 这里会多读一次IO， 会不会有性能方面的影响？
+                if (resultBufferLength > 0 ) { //wangxc, 表示还有最终的结果没有取走。
+                    return true;
+                }
                 return false;
             }
-            if (! hasWhiteSpanceInLeft(ioBuffer, offset) ) {
-                ioBuffer = new char[IO_BUFFER_SIZE];
-                in.reset();
-//                int dataLen = in.read(ioBuffer, lastBlankIndex + 1, ioBuffer.length - lastBlankIndex + 1); //先读出来，
-                in.skip(lastBlankIndex + 1);
-                int dataLen = in.read(ioBuffer); //先读出来，
-                if (dataLen > 0 ) {
-                    //由于IO_BUFFER_SIZE很大（4096），没有一个单词的长度超过这个， 就先不考虑buffer里只有半截词的情况。
-                    for (int i=0;i<dataLen;i++) {
-                        char c = ioBuffer[i];
-                        if (Character.isWhitespace(c)) {
-                            offset = i;
-                            lastBlankIndex = offset + bufferIndex;
-                            return  true;
-                        }
-                    }
 
-                }
-            } else {
-                for (int i=bufferIndex;i<ioBuffer.length;i++) {
-                    char c = ioBuffer[i];
-                    if (Character.isWhitespace(c)) {
-                        offset = i;
-                        lastBlankIndex = offset + bufferIndex;
-
-                        return  true;
-                    }
-                }
+            char c = ioBuffer[bufferIndex ++];
+            if (! Character.isWhitespace(c)) {
+                resultBuffer[resultBufferLength++] = c;
+            } else { //wangxc 如果是空白时， 返回true
+                return true;
             }
 
-
-        } else {
-            int dataLen = in.read(ioBuffer, lastBlankIndex, ioBuffer.length); //先读出来，
-
-            if (dataLen > 0 ) {
-                //由于IO_BUFFER_SIZE很大（4096），没有一个单词的长度超过这个， 就先不考虑buffer里只有半截词的情况。
-                for (int i=0;i<dataLen;i++) {
-                    char c = ioBuffer[i];
-                    if (Character.isWhitespace(c)) {
-                        offset = i;
-                        lastBlankIndex = offset + bufferIndex;
-                        return  true;
-                    }
-                }
-
-            }
+//            if (bufferIndex == dataLen) {
+//                return true;
+//            }
         }
-        return  false;
     }
-
-
-    boolean hasWhiteSpanceInLeft(char[] ioBuffer, int offset) {
-        String left = new String(ioBuffer, offset+1, ioBuffer.length - offset - 1);
-        return left.contains(" ");
-    }
-
-//    @Override
-//    public boolean incrementToken() throws IOException {
-//        if (! bufferEmpty(ioBuffer)) {//缓存里已有时， 直接从缓存里读。
-//            if (ioBuffer[offset] == BLANK_CHAR) {
-//                return false;
-//            }
-//
-//            for (int i=bufferIndex;i<ioBuffer.length;i++) {
-//                char c = ioBuffer[i];
-////                if (Character.isWhitespace(c) || i == ioBuffer.length - 1) {
-//                if (Character.isWhitespace(c) || i == ioBuffer.length - 1 || c == BLANK_CHAR) {
-//                    offset = i;
-//                    return  true;
-//                }
-//            }
-//        } else {
-//            int dataLen = in.read(ioBuffer); //先读出来，
-//
-//            if (dataLen > 0 ) {
-//                //由于IO_BUFFER_SIZE很大（4096），没有一个单词的长度超过这个， 就先不考虑buffer里只有半截词的情况。
-//                for (int i=0;i<dataLen;i++) {
-//                    char c = ioBuffer[i];
-//                    if (Character.isWhitespace(c)) {
-//                        offset = i;
-//                        return  true;
-//                    }
-//                }
-//
-//            }
-//        }
-//        return  false;
-//    }
-
-
-    static boolean bufferEmpty(char[] buffer) {
-        return buffer[0] == BLANK_CHAR;
-    }
-
 
     @Override
     public Token nextToken() {
-        String result = new String(ioBuffer, bufferIndex, offset - bufferIndex);
-        bufferIndex = offset + 1;
+        String result = new String(resultBuffer, 0, resultBufferLength);
+        resultBufferLength = 0;
         return new Token(result);
-    }
-
-    //TODO, 这个不明白有什么特殊的作用， 先抄过来。 FIXME
-    protected boolean isTokenChar(char c) {
-        return !Character.isWhitespace(c);
     }
 
     public static void setIoBufferSize(int ioBufferSize) {
--- src/test/java/cn/sluk3r/luImpl/analysis/TestWhitespaceTokenizer.java
+++ src/test/java/cn/sluk3r/luImpl/analysis/TestWhitespaceTokenizer.java
@@ -14,44 +14,25 @@ import org.junit.Test;
 public class TestWhitespaceTokenizer {
     @Test
     public void testNormal() throws IOException {
-        WhitespaceAnalyzer whitespaceAnalyzer = new WhitespaceAnalyzer();
-
         String content = "C embedded developers wanted";
-        StringReader reader = new StringReader(content);
-
-        TokenStream stream = whitespaceAnalyzer.tokenStream(null, reader);
-
-        String[] targets = content.split(" ");
-
-        int i=0;
-        while( stream.incrementToken()) {
-            Token token = stream.nextToken();
-
-            Pattern whitespace = Pattern.compile("\\s");
-            Matcher matcher = whitespace.matcher(token.getContent());
-            assertFalse("whitespace char should not in token, now the content is: " + token.getContent(), matcher.find());
-            assertEquals(String.format("whitespace-splitted[%s] should be equals to self splitted[%s]", token.getContent(), targets[i]), targets[i], token.getContent());
-            i++;
-
-            System.out.println("\t\t\twhitespace-splitted: " + token.getContent());
-        }
-        assertEquals(String.format("分词出来的个数[%s]应该跟目标[%s]一致", i, targets.length), targets.length, i);
+        doTest(content);
     }
 
-
     @Test
-    public void testIncompleteToken() throws IOException {
+    public void testExceedIOBuffer() throws IOException {
         String content = "C embedded developers wanted";
         int firstBlankIndex = content.indexOf(" ");
         WhitespaceTokenizer.setIoBufferSize(firstBlankIndex + 1 + 2);
 
-        StringReader reader = new StringReader(content);
+        doTest(content);
+    }
 
+    private void doTest(String content) throws IOException {
+        StringReader reader = new StringReader(content);
         WhitespaceAnalyzer whitespaceAnalyzer = new WhitespaceAnalyzer();
         TokenStream stream = whitespaceAnalyzer.tokenStream(null, reader);
 
         String[] targets = content.split(" ");
-
         int i=0;
         while( stream.incrementToken()) {
             Token token = stream.nextToken();
@@ -61,9 +42,8 @@ public class TestWhitespaceTokenizer {
             assertFalse("whitespace char should not in token, now the content is: " + token.getContent(), matcher.find());
             assertEquals(String.format("whitespace-splitted[%s] should be equals to self splitted[%s]", token.getContent(), targets[i]), targets[i], token.getContent());
             i++;
-
-            System.out.println("\t\t\twhitespace-splitted: " + token.getContent());
         }
         assertEquals(String.format("分词出来的个数[%s]应该跟目标[%s]一致", i, targets.length), targets.length, i);
     }
+
 }
